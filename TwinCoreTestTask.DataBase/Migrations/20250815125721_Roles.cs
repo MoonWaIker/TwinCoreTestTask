a9669fef9e49677dc1d4068983f5890d7724da6e
Michael <muhinmihajlo40@gmail.com>
2025-08-15 16:03:50 +0300

    Added migrations for roles

---
+++ TwinCoreTestTask.DataBase/Migrations/20250815125721_Roles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TwinCoreTestTask.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class Roles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "0", "c17bb403-02be-479d-aeb3-7934bf026a88", "User", "USER" },
+                    { "1", "d9d3e5e7-34b3-4513-bc89-1b8dc6c90cb8", "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1");
+        }
+    }
+}
